5d651736cce33427a84ec42a5a326573f38a3c89
Yople2005 <yoplethepsycho@gmail.com>
2025-01-27 21:11:53 +0630

    m

---
+++ Chapter-3/3-15/DateTest.java
@@ -0,0 +1,16 @@
+package chapter2;
+
+public class DateTest {
+    public static void main(String[] args) {
+        Date date = new Date(1, 1, 2025);
+
+        date.displayDate();
+
+        date.setMonth(1);
+        date.setDay(22);
+        date.setYear(2005);
+
+        System.out.println("Modified Date:");
+        date.displayDate();
+    }
+}
