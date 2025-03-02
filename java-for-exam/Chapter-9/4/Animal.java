a8cd20f64e461b58ec4363f43d511daa4b95e05e
Yople2005 <yoplethepsycho@gmail.com>
2025-03-02 13:45:36 +0630

    m

---
+++ java-for-exam/Chapter-9/4/Animal.java
@@ -0,0 +1,7 @@
+package Java.Chapter9;
+
+public class Animal {
+    public void move() {
+        System.out.println("Animal moves");
+    }
+}
---
+++ java-for-exam/Chapter-9/4/Cheetah.java
@@ -0,0 +1,8 @@
+package Java.Chapter9;
+
+public class Cheetah extends Animal {
+    @Override
+    public void move() {
+        System.out.println("This cheetah is running!");
+    }
+}
