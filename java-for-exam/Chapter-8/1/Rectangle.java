357737f8edaa9fca6be31177a7e53c19f57e654c
Yople2005 <yoplethepsycho@gmail.com>
2025-03-08 21:46:38 +0630

    m

--- java-for-exam/Chapter-8/1/Rectangle.java
+++ java-for-exam/Chapter-8/1/Rectangle.java
@@ -13,12 +13,12 @@ public class Rectangle {
         setWidth(width);
     }
 
-    // SETTERS
+    // Setters
     public void setLength(double length) {
         if (length >= 0.0f && length <= 20.0f) {
             this.length = length;
         } else {
-            throw new IllegalArgumentException("Length must be between 0.0 and 20.0");
+            throw new IllegalArgumentException("length must be between 0.0 and 20.0");
         }
     }
 
@@ -26,11 +26,11 @@ public class Rectangle {
         if (width >= 0.0f && width <= 20.0f) {
             this.width = width;
         } else {
-            throw new IllegalArgumentException("Width must be between 0.0 and 20.0");
+            throw new IllegalArgumentException("width must be between 0.0 and 20.0");
         }
     }
 
-    // GETTERS
+    // Getters
     public double getLength() {
         return this.length;
     }
@@ -49,9 +49,12 @@ public class Rectangle {
         return length * width;
     }
 
-    // Convert length, width, area and perimeter to string
+    // Convert length, width, area, and perimeter to string
+    @Override
     public String toString() {
-        return String.format("Width: %.2f\nLength: %.2f\nArea: %.2f\nPerimeter: %.2f\n", 
-                             getLength(), getWidth(), getArea(), getPerimeter());
+        return String.format(
+            "Width: %.2f\nLength: %.2f\nArea: %.2f\nPerimeter: %.2f\n",
+            getWidth(), getLength(), getArea(), getPerimeter()
+        );
     }
 }
